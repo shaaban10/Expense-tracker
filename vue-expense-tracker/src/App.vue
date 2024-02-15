103f4cf8985bdf067270351dfb1124c69b59675c
imshaaban <shaabanalahmed20gmail.com>
2024-02-15 19:10:04 +0100

    first files

---
+++ vue-expense-tracker/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <Header />
+  <div class="container">
+  <Balance />
+  </div>
+  <div class="inc-exp-container">
+    <IncomeExpensence />
+  </div>
+  <TransactionsList />
+  <div class="form-control">
+<AddTransactions />
+    <div class="form-control">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue'
+import IncomeExpensence from './components/IncomeExpensence.vue'
+import TransactionsList from './components/TransactionsList.vue'
+import AddTransactions from './components/AddTransactions.vue'
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpensence,
+    TransactionsList,
+    AddTransactions
+
+
+  },
+};
+</script>
